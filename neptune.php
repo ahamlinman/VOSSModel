a63ecf00c666abdf3640e1faf8274868fc0464fc
Alex Hamlin <alexh1995@gmail.com>
2015-09-24 23:04:23 -0400

    Reformat and fix Neptune page

--- neptune.php
+++ neptune.php
@@ -3,120 +3,129 @@
 
 <?php $orb_class = "active neptune"; ?>
 
-	<head>
-		<title>VOSS Model: Neptune</title>
-		<?php include('fragments/head.html'); ?>
-	</head>
-	
-	<body>
+    <head>
+        <title>VOSS Model: Neptune</title>
+        <?php include('fragments/head.html'); ?>
+    </head>
+
+    <body>
 
         <?php include('fragments/menu.html'); ?>
-	<div id="mainContainer">		
-		<div id = "wrapper">
-		
-		<section class="hero" style="background: url('images/Neptune/Cover.jpg');">
-            <div class="overlay">
-                <div class="container">
-                    <h1>Neptune</h1>
+            <div id="mainContainer">
+                <div id="wrapper">
+
+                    <section class="hero" style="background: url('images/Neptune/Cover.jpg');">
+                        <div class="overlay">
+                            <div class="container">
+                                <h1>Neptune</h1>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <h2>Introduction</h2>
+                                </div>
+                            </div>
+                            
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-7">
+                                    <p>Neptune is the eighth planet from the Sun and is commonly known for its deep blue color. Neptune was discovered in 1846 through mathematical calculations. A mathematician realized that there had to be another planet beyond Uranus, because something was influencing the motion of Uranus. Astronomers all over Europe continued searching for this hypothetical eighth planet, especially England’s John Couch Adams and France’s Urbain Le Verrier. Finally Johann Gottfried Galle, a German astronomer, used all the previously formed calculations to find Neptune. Because of all the different astronomers contributing to this discovery, there was a rivalry to decide who should officially get credit for finding Neptune. Today, both Le Verrier and Adams are recognized. Neptune can only be seen through a telescope. It is named after the Greek god Poseidon, the god of the sea, because of the blue surface.</p>
+                                </div>
+                                
+                                <div class="col-xs-12 col-sm-5">
+                                    <img class="img-responsive img-rounded" src="images/Neptune/neptune.jpg">
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <h2>Size &amp; Scale</h2>
+                                </div>
+                            </div>
+                            
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-7 col-sm-push-5">
+                                    <p>Neptune has a mass about 17 times greater than the Earth's. Neptune’s inner core alone is the mass of the Earth. Because it is so much farther from the Sun, Neptune receives only 1/900 of the solar energy that reaches the Earth. However, Neptune does have its own heat source and expels 2.7 times more energy than it receives.</p>
+                                </div>
+                                
+                                <div class="col-xs-12 col-sm-5 col-sm-pull-7">
+                                    <img class="img-responsive img-rounded" src="images/Neptune/opt/Neptune_Int.jpg">
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <h2>Environment/Terrain</h2>
+                                </div>
+                            </div>
+                            
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-7">
+                                    <p>Neptune is the smaller of our solar system's two ice giants. It is a very cold planet, with an average temperature of -329 degrees Fahrenheit. Neptune is made up mainly of hydrogen, helium, water, silicates, and methane. The methane gas absorbs all of the red light, which gives the planet's surface a blue glow. Thick clouds cover Neptune’s surface and move at very high speeds of up to 1,300 mph. These winds created the largest storm ever recorded, called the Great Dark Spot of 1989, which lasted about five years. Neptune also has very thin rings made up of ice particles and dust grains that could be clumped together by a carbon-based substance.</p>
+                                </div>
+                                
+                                <div class="col-xs-12 col-sm-5">
+                                    <img class="img-responsive img-rounded" src="images/Neptune/Neptune's Blue-green Atmosphere.jpg">
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <h2>Orbiting Bodies</h2>
+                                </div>
+                            </div>
+                            
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-7 col-sm-push-5">
+                                    <p>Neptune has thirteen known satellites. Triton is Neptune’s largest satellite. It orbits in a direction opposite to that of its planet, and is the only known satellite to behave this way. Volcanoes are very active on its surface, and Triton’s surface temperature is -390 degrees Fahrenheit, which may be the coldest temperature in the entire Solar System.</p>
+                                </div>
+                                
+                                <div class="col-xs-12 col-sm-5 col-sm-pull-7">
+                                    <img class="img-responsive img-rounded" src="images/Neptune/feature.jpg">
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <h2>Missions</h2>
+                                </div>
+                            </div>
+                            
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-7">
+                                    <p>Voyager 2 in 1989 revealed many new details about Triton. For example, it found that its surface resembles the rigid and tough rind of a cantaloupe. It also captured many instances of volcanoes on Triton and the frosty plumes of liquid methane/nitrogen that erupted from them, drifting in the atmosphere. It returned the first close-up images of Neptune and its satellites. This space probe took 12 years to reach Neptune.</p>
+                                </div>
+                                
+                                <div class="col-xs-12 col-sm-5">
+                                    <img class="img-responsive img-rounded" src="images/Neptune/PIA00057_modest.jpg">
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
                 </div>
             </div>
-        </section>
-        
-        <section style="color: white;">
-           <div class="container">
-           <div class="row">
-               <div class="col-xs-12">
-                   <h2>Introduction</h2>
-               </div>
-           </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-7">
-				<p>Neptune is the eighth planet from the Sun and is commonly known for its deep blue color. Neptune was discovered in 1846 through mathematical calculations. A mathematician realized that there had to be another planet beyond the Uranus, because something was influencing the motion of Uranus. Astronomers all over Europe continued searching for this hypothetical eighth planet, especially England’s John Couch Adams and France’s Urbain Le Verrier. Finally Johann Gottfried Galle, a German astronomer, used all the previously formed calculations to find Neptune. Because of all the different astronomers contributing to this discovery, there was a rivalry to decide who should officially get credit for finding Neptune. Today, both Le Verrier and Adams are recognized. Neptune can only be seen through a telescope. It is named after the Greek god Poseidon, the god of the sea, because of the blue surface. </p>
-                </div>
-                <div class="col-xs-12 col-sm-5">
-                    <img class="img-responsive img-rounded" src="images/Neptune/neptune.jpg">
-                </div>
-            </div>
-            </div>
-        </section>
-		
-		<section style="color: white;">
-           <div class="container">
-           <div class="row">
-               <div class="col-xs-12">
-                   <h2>Size & Scale</h2>
-               </div>
-           </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-7 col-sm-push-5">
-				<p>Neptune has a mass about 17 times greater than the Earth's, meaning that about four Earths can fit inside of it. Neptune’s inner core alone is the mass of the Earth. Because of being so much farther from the Sun, Neptune receives only 1/900 of the solar energy that reaches the Earth. However, Neptune does have its own heat source and expels 2.7 times more energy than it receives. </p>
-                </div>
-                <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                    <img class="img-responsive img-rounded" src="images/Neptune/opt/Neptune_Int.jpg">
-                </div>
-            </div>
-            </div>
-        </section>
-        
-        <section style="color: white;">
-           <div class="container">
-           <div class="row">
-               <div class="col-xs-12">
-                   <h2>Environment/Terrain</h2>
-               </div>
-           </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-7">
-				<p>Neptune is the smaller of our solar system's two ice giants. Neptune is a very cold planet, with an average temperature of -329 degrees Fahrenheit. Neptune is made up mainly of hydrogen, helium, water, silicates, and methane. The methane gas absorbs all of the red light, which gives Neptune’s surface a blue glow. Thick clouds cover Neptune’s surface and move at very high speeds of up to 1,300 mph. These winds created the largest storm ever recorded, called the Great Dark Spot of 1989, which lasted about five years. Neptune also has very thin rings made up of ice particles and dust grains that could be clumped together by a carbon-based substance. </p>
-				</div>
-                <div class="col-xs-12 col-sm-5">
-                    <img class="img-responsive img-rounded" src="images/Neptune/Neptune's Blue-green Atmosphere.jpg">
-                </div>
-            </div>
-            </div>
-        </section>
-        
-        <section style="color: white;">
-           <div class="container">
-           <div class="row">
-               <div class="col-xs-12">
-                   <h2>Orbiting Bodies</h2>
-               </div>
-           </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-7 col-sm-push-5">
-				<p>Neptune has thirteen known satellites. Triton is Neptune’s largest satellite. It orbits in a direction opposite to that of its planet, and is the only known satellite to behave this way. Volcanoes are very active on its surface, and Triton’s surface temperature is -390 degrees Fahrenheit, which may be the coldest temperature in the entire Solar System.</p>
-                </div>
-                <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                    <img class="img-responsive img-rounded" src="images/Neptune/feature.jpg">
-                </div>
-            </div>
-            </div>
-        </section>
-        
-        <section style="color: white;">
-           <div class="container">
-           <div class="row">
-               <div class="col-xs-12">
-                   <h2>Missions</h2>
-               </div>
-           </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-7">
-				<p>Voyager 2 in 1989 revealed many new details about Triton. For example, it found that its surface resembles the rigid and tough rind of a cantaloupe. It also captured many instances of volcanoes on Triton and the frosty plumes of liquid methane/nitrogen the erupted from them, drifting in the atmosphere. It returned the first close-up images of Neptune and its satellites. This space probe took 12 years to reach Neptune.</p>
-                </div>
-                <div class="col-xs-12 col-sm-5">
-                    <img class="img-responsive img-rounded" src="images/Neptune/PIA00057_modest.jpg">
-                </div>
-            </div>
-            </div>
-        </section>
-		       
-        
-		</div>
-		
-	</div>
-	<script src="js/menuFunctions.js"></script>
-	</body>
-	
+            
+            <script src="js/menuFunctions.js"></script>
+    </body>
+
 </html>
