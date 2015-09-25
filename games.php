50fd2758e372f4029247ce763907c673f742e6d3
Alex Hamlin <alexh1995@gmail.com>
2015-09-24 22:02:19 -0400

    Reformat games page

--- games.php
+++ games.php
@@ -3,71 +3,55 @@
 
 <?php $orb_class = "inactive"; ?>
 
-	<head>
-		<title>VOSS Model: Games</title>
-		<?php include('fragments/head.html'); ?>
-	</head>
-	
-	<body>	
+    <head>
+        <title>VOSS Model: Games</title>
+        <?php include('fragments/head.html'); ?>
+    </head>
+
+    <body>
         <?php include('fragments/menu.html'); ?>
-		<div id="mainContainer">
-		    <div id="wrapper">
-				<?php include('fragments/load.html'); ?>
-		    
-		                <section class="hero" style="background: none;">
-		                    <div class="overlay">
-		                        <div class="container">
-		                            <h1>Interactive Games</h1>
-		                        </div>
-		                    </div>
-		                </section>
-		                
-		                <section style="color: white;">
-		                    <div class="container">
-		                        <div class="row">
-		                            <div class="col-xs-12 col-sm-6">
-		                                <p>
-		                                    <a href="http://www.nasa.gov/audience/forkids/kidsclub/flash/clubhouse/index.html#.VFvnZovF88F">
-		                                        <h2 class="text-center">NKC Clubhouse</h2>
-		                                        <img class="img-responsive" src="images/games/NKC clubhouse.jpg">
-		                                    </a>
-		                                </p>
-		                                <p>Go on an out of this world journey, exploring games and interactive activities with the Clubhouse Commander, Nebula. Learn all about our solar system, NASA, astronauts, spaceships, and so much more!</p>
-		                            </div>
-		                            <div class="col-xs-12 col-sm-6">
-		                                <p>
-		                                    <a href="http://spaceplace.nasa.gov/menu/solar-system/">
-		                                        <h2 class="text-center">NASA Space Place</h2>
-		                                        <img class="img-responsive" src="images/games/NASA spaceplace.jpg">
-		                                    </a>
-		                                </p>
-		                                <p>Explore the solar system through games and various arts and crafts activites.</p>
-		                            </div>
-		                        </div>
-		                    </div>
-		                </section>
-		        <!--
-		                <div class = "content">
-		            <img src = "INSERT THE IMAGE HERE">
-		            <div class="text">
-		                <h2>Game #3</h2>
-		                
-		                <p> PUT THE DESCRIPTION HERE</p>
-		            </div>
-		        </div>
-		        
-		                <div class = "content">
-		            <img src = "INSERT THE IMAGE HERE">
-		            <div class="text">
-		                <h2>Game #4</h2>
-		                
-		                <p> PUT THE DESCRIPTION HERE</p>
-		            </div>
-		        </div>-->
-		    </div>
-		</div>
-		<script src="js/menuFunctions.js"></script>
-	</body>
-	
+            <div id="mainContainer">
+                <div id="wrapper">
+                    <?php include('fragments/load.html'); ?>
+
+                        <section class="hero" style="background: none;">
+                            <div class="overlay">
+                                <div class="container">
+                                    <h1>Interactive Games</h1>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-6">
+                                        <p>
+                                            <a href="http://www.nasa.gov/audience/forkids/kidsclub/flash/clubhouse/index.html#.VFvnZovF88F">
+                                                <h2 class="text-center">NKC Clubhouse</h2>
+                                                <img class="img-responsive" src="images/games/NKC clubhouse.jpg">
+                                            </a>
+                                        </p>
+                                        
+                                        <p>Go on an out of this world journey, exploring games and interactive activities with the Clubhouse Commander, Nebula. Learn all about our solar system, NASA, astronauts, spaceships, and so much more!</p>
+                                    </div>
+                                    
+                                    <div class="col-xs-12 col-sm-6">
+                                        <p>
+                                            <a href="http://spaceplace.nasa.gov/menu/solar-system/">
+                                                <h2 class="text-center">NASA Space Place</h2>
+                                                <img class="img-responsive" src="images/games/NASA spaceplace.jpg">
+                                            </a>
+                                        </p>
+                                        
+                                        <p>Explore the solar system through games and various arts and crafts activites.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                </div>
+            </div>
+            <script src="js/menuFunctions.js"></script>
+    </body>
+
 </html>
-	
