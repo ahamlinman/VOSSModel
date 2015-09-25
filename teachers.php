3c2bf5e63af1a08fb942d412acf3b2f1f9c4616c
Alex Hamlin <alexh1995@gmail.com>
2015-09-24 21:56:57 -0400

    Reformat teachers page

--- teachers.php
+++ teachers.php
@@ -3,98 +3,87 @@
 
 <?php $orb_class = "inactive"; ?>
 
-<head>
-    <title>VOSS Model: For the Teacher</title>
-    <?php include('fragments/head.html'); ?>
-</head>
+    <head>
+        <title>VOSS Model: For the Teacher</title>
+        <?php include('fragments/head.html'); ?>
+    </head>
 
-<body>
-    <?php include( 'fragments/menu.html'); ?>
-<div id="mainContainer">    
-	<div id="wrapper">
-				<?php include('fragments/load.html'); ?>
-        <section class="hero" style="background: url('images/Teacher/Background.jpg');">
-            <div class="overlay">
-                <div class="container">
-                    <h1>Teaching Resources for the Classroom</h1>
+    <body>
+        <?php include('fragments/menu.html'); ?>
+            <div id="mainContainer">
+                <div id="wrapper">
+                    <?php include('fragments/load.html'); ?>
+
+                        <section class="hero" style="background: url('images/Teacher/Background.jpg');">
+                            <div class="overlay">
+                                <div class="container">
+                                    <h1>Teaching Resources for the Classroom</h1>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <h2>The Small Scale Model</h2>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-7">
+                                        <p>This board acts as an educational small-scale model of the large VOSS model in Discovery Park. The purpose is to teach students about the planets and their placement around the sun in space. The model consists of a small scale board, interactive children’s story and macro-scale planet model. </p>
+                                        <p>The board uses the same Fibonacci sequence as the VOSS model in Discovery Park, but is a smaller interactive representation of the planets compared to each other. The board contains different facts about each planet such as the weight, force of gravity, surface texture, color, etc. The sizes of the planets are to scale with each other. The sun, however, is to half of that scale. The distance between the sun and the planets along the Fibonacci spiral are also to scale with their actual distance. The height of the rods is also symbolic as it represents the mass of the planets.</p>
+                                    </div>
+
+                                    <div class="col-xs-12 col-sm-5">
+                                        <img class="img-responsive img-rounded" src="images/Teacher/Students.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-7 col-sm-push-5">
+                                        <h2>Interactive Children's Story</h2>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-7 col-sm-push-5">
+                                        <p>This book is a fun and educational story about Riley the Rocket who travels through the solar system. Throughout the rocket’s journey, information is given about each of the planets along with other astronomical objects. The information includes but is not limited to their size, color, texture and force of gravity. It is designed to be both an informative and an entertaining read.</p>
+
+                                    </div>
+                                    <div class="col-xs-12 col-sm-5 col-sm-pull-7">
+                                        <img class="img-responsive img-rounded" src="images/Teacher/rocket.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <h2>Macro-Scale Model</h2>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-7">
+                                        <p>This model is simply a scalar representation of the distances between the planets and the sun. It consists of a rope with the planet names distributed along the rope. The distance between each planet is to scale to the actual distance. The planetary rods that come with the small scale model can be placed on top of a cone with the aid of a washer and placed on top of their respective place on the rope. This is not necessary, however, and other things can be used to represent the place of the planets along the rope. An alternative suggestion would be to have a student stand in representation of the planets. There is also a worksheet that can be used to change the scale if necessary.
+                                    </div>
+
+                                    <div class="col-xs-12 col-sm-5">
+                                        <img class="img-responsive img-rounded" src="images/Teacher/MacroModel2.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
                 </div>
             </div>
-        </section>
 
-        <section style="color: white;">
-           <div class="container">
-           <div class="row">
-               <div class="col-xs-12">
-                   <h2>The Small Scale Model</h2>
-               </div>
-           </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-7">
-                    <p>This board acts as an educational small-scale model of the large VOSS model in Discovery Park. The purpose is to teach students about the planets and their placement around the sun in space. The model consists of a small scale board, interactive children’s story and macro-scale planet model. </p>
-					<br><br>
-					<p>The board uses the same Fibonacci sequence as the VOSS model in Discovery Park, but is a smaller interactive representation of the planets compared to each other. The board contains different facts about each planet such as the weight, force of gravity, surface texture, color, etc. The sizes of the planets are to scale with each other. The sun, however, is to half of that scale. The distance between the sun and the planets along the Fibonacci spiral are also to scale with their actual distance. The height of the rods is also symbolic as it represents the mass of the planets.</p>
-
-				</div>
-				
-
-                <div class="col-xs-12 col-sm-5">
-                    <img class="img-responsive img-rounded" src="images/Teacher/Students.jpg">
-                </div>
-            </div>
-            </div>
-        </section>
-
-        <section style="color: white;">
-            <div class="container">
-               <div class="row">
-                   <div class="col-xs-12 col-sm-7 col-sm-push-5">
-                       <h2>Interactive Children's Story</h2>
-                   </div>
-               </div>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-7 col-sm-push-5">
-					   <p>This book is a fun and educational story about Riley the Rocket who travels through the solar system. Throughout the rocket’s journey, information is given about each of the planets along with other astronomical objects. The information includes but is not limited to their size, color, texture and force of gravity. It is designed to be both an informative and an entertaining read.</p>
-
-						</div>
-                    <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                        <img class="img-responsive img-rounded" src="images/Teacher/rocket.jpg">
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section style="color: white;">
-            <div class="container">
-               <div class="row">
-                   <div class="col-xs-12">
-                       <h2>Macro-Scale Model</h2>
-                   </div>
-               </div>
-                <div class="row">
-				
-				
-                    <div class="col-xs-12 col-sm-7">
-				
-                        <p>This model is simply a scalar representation of the distances between the planets and the sun. It consists of a rope with the planet names distributed along the rope. The distance between each planet is to scale to the actual distance. The planetary rods that come with the small scale model can be placed on top of a cone with the aid of a washer and placed on top of their respective place on the rope. This is not necessary, however, and other things can be used to represent the place of the planets along the rope. An alternative suggestion would be to have a student stand in representation of the planets. There is also a worksheet that can be used to change the scale if necessary.
-						</div>
-                    <div class="col-xs-12 col-sm-5">
-                        <img class="img-responsive img-rounded" src="images/Teacher/MacroModel2.jpg">
-                    </div>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-7 col-sm-push-5">
-                        <p></p>
-                    </div>
-                    <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                        <img class="img-responsive img-rounded" src="">
-                    </div>
-                </div>
-            </div>
-        </section>
-		
-</div>
-<script src="js/menuFunctions.js"></script>
-</body>
+            <script src="js/menuFunctions.js"></script>
+    </body>
 
 </html>
