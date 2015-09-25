ff90ae1fc9dd5b02973d52ea8ab099b4b53a3770
Alex Hamlin <alexh1995@gmail.com>
2015-09-24 22:48:35 -0400

    Reformat and fix Asteroid Belt

--- asteroid.php
+++ asteroid.php
@@ -3,116 +3,116 @@
 
 <?php $orb_class = "active asteroid"; ?>
 
-<head>
-    <title>VOSS Model: Asteroid Belt</title>
-    <?php include('fragments/head.html'); ?>
-</head>
+    <head>
+        <title>VOSS Model: Asteroid Belt</title>
+        <?php include('fragments/head.html'); ?>
+    </head>
 
-<body>
-    <?php include( 'fragments/menu.html'); ?>
-    <div id="mainContainer">
-        <div id="wrapper">
-				<?php include('fragments/load.html'); ?>
-				<?php include('fragments/load.html'); ?>
-            <section class="hero" style="background: url('images/asteroid/Cover.jpg');">
-                <div class="overlay">
-                    <div class="container">
-                        <h1>Asteroid Belt</h1>
-                    </div>
-                </div>
-            </section>
-        
-            <section style="color: white;">
-               <div class="container">
-               <div class="row">
-                   <div class="col-xs-12">
-                       <h2>Introduction</h2>
-                   </div>
-               </div>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-7">
-                        <p>A region of the solar system out beyond Mars contains the majority of the asteroids. This is called the Asteroid Belt. The four largest asteroids in the belt are called Ceres, Vesta, Pallas, and Hygiea. These large asteroids make up about half of the mass of the entire asteroid belt. Scientists like to say that if you combined all of these rocky asteroids together, it would make a small planet a bit smaller than Earth's moon.  </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-5">
-                        <img class="img-responsive img-rounded" src="images/asteroid/opt/PHA.png">
-                    </div>
-                </div>
-                </div>
-            </section>
-        
-            <section style="color: white;">
-                <div class="container">
-                   <div class="row">
-                       <div class="col-xs-12">
-                           <h2>Where is the Asteroid Belt?</h2>
-                       </div>
-                   </div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-7 col-sm-push-5">
-                            <p>The Asteroid Belt is between the orbits of Mars and Jupiter. The space between asteroids is large enough that when standing on a given asteroid, it would be impossible to see any other object.</p>
-                        </div>
-                        <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                            <img class="img-responsive img-rounded" src="images/asteroid/Asteroids.jpg">
-                        </div>
-                    </div>
-                </div>
-            </section>
-        
-            <section style="color: white;">
-                <div class="container">
-                   <div class="row">
-                       <div class="col-xs-12">
-                           <h2>What Exactly IS an Asteroid?</h2>
-                       </div>
-                   </div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-7">
-                            <p>An asteroid is a small, rocky solar bodies that orbit in space. Scientists refer to these bodies as minor planets. There are millions upon millions of asteroids that are commonly grouped based on their composition. Asteroids are mainly made up of materials that are left over from the creation of the inner solar system. There are three types of composition classes. C-types are made of clay and silicate rocks. S-types are made of silicate rocks and nickel-iron mixtures. M-types are made of metallic nickel-iron. The different compositions describe how far from the Sun they were formed. </p>
-                        </div>
-                        <div class="col-xs-12 col-sm-5">
-                            <img class="img-responsive img-rounded" src="images/asteroid/opt/Asteroid_Collision.jpg">
-                        </div>
-                    </div>
-                    <br>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-7 col-sm-push-5">
-                            <p></p>
-                        </div>
-                        <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                            <img class="img-responsive img-rounded" src="">
-                        </div>
-                    </div>
-                </div>
-            </section>
-                
-                <section style="color: white;">
-                <div class="container">
-                   <div class="row">
-                       <div class="col-xs-12">
-                           <h2>Facts and Figures About the Asteroid Belt</h2>
-                       </div>
-                   </div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-7 col-sm-push-5">
-                            <p>1. Some asteroids are solid objects of rock and stone, while others are "rubble piles." <br>
-                                2. Very few asteroids are extremely large. Most are actually the size of pebbles. <br>
-                                3. Ceres is also defined as a dwarf planet. It is the largest dwarf planet in the solar system. <br>
-                                4. Scientists know of more than 7,000 asteroids! <br>
-                                5. An asteroid hitting Earth contributed to the extinction of dinosaurs 65 million years ago. <br>
-                                6. Asteroids about the size of a football field impact Earth about once every 2,000 years. <br>
-                                7. Some asteroids are large enough that they actually contain moons of their own. <br>
-                                </p>
-                        </div>
-                        <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                            <img class="img-responsive img-rounded" src="images/asteroid/opt/White_Dwarf_Asteroid_br.jpg">
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    </div>
+    <body>
+        <?php include('fragments/menu.html'); ?>
+            <div id="mainContainer">
+                <div id="wrapper">
+                    <?php include('fragments/load.html'); ?>
+                        <?php include('fragments/load.html'); ?>
+                            <section class="hero" style="background: url('images/asteroid/Cover.jpg');">
+                                <div class="overlay">
+                                    <div class="container">
+                                        <h1>Asteroid Belt</h1>
+                                    </div>
+                                </div>
+                            </section>
 
-        <script src="js/menuFunctions.js"></script>
-</body>
+                            <section>
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <h2>Introduction</h2>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-7">
+                                            <p>A region of the solar system out beyond Mars contains the majority of the asteroids. This is called the Asteroid Belt. The four largest asteroids in the belt are called Ceres, Vesta, Pallas, and Hygiea. These large asteroids make up about half of the mass of the entire asteroid belt. Scientists like to say that if you combined all of these rocky asteroids together, it would make a planet a bit smaller than Earth's moon.</p>
+                                        </div>
+
+                                        <div class="col-xs-12 col-sm-5">
+                                            <img class="img-responsive img-rounded" src="images/asteroid/opt/PHA.png">
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+
+                            <section>
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <h2>Where is the Asteroid Belt?</h2>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-7 col-sm-push-5">
+                                            <p>The Asteroid Belt is between the orbits of Mars and Jupiter. The space between asteroids is large enough that when standing on a given asteroid, it would be impossible to see any other object.</p>
+                                        </div>
+
+                                        <div class="col-xs-12 col-sm-5 col-sm-pull-7">
+                                            <img class="img-responsive img-rounded" src="images/asteroid/Asteroids.jpg">
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+
+                            <section>
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <h2>What Exactly <em>is</em> an Asteroid?</h2>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-7">
+                                            <p>Asteroids are small, rocky solar bodies that orbit in space. Scientists refer to these bodies as minor planets. There are millions upon millions of asteroids that are commonly grouped based on their composition. Asteroids are mainly made up of materials that are left over from the creation of the inner solar system. There are three types of composition classes. C-types are made of clay and silicate rocks. S-types are made of silicate rocks and nickel-iron mixtures. M-types are made of metallic nickel-iron. The different compositions describe how far from the Sun they were formed. </p>
+                                        </div>
+
+                                        <div class="col-xs-12 col-sm-5">
+                                            <img class="img-responsive img-rounded" src="images/asteroid/opt/Asteroid_Collision.jpg">
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+
+                            <section>
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <h2>Facts and Figures About the Asteroid Belt</h2>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-7 col-sm-push-5">
+                                            <ol>
+                                                <li>Some asteroids are solid objects of rock and stone, while others are "rubble piles."</li>
+                                                <li>Very few asteroids are extremely large. Most are actually the size of pebbles.</li>
+                                                <li>Ceres is also defined as a dwarf planet. It is the largest dwarf planet in the solar system.</li>
+                                                <li>Scientists know of more than 7,000 asteroids!</li>
+                                                <li>An asteroid hitting Earth contributed to the extinction of dinosaurs 65 million years ago.</li>
+                                                <li>Asteroids about the size of a football field impact Earth about once every 2,000 years.</li>
+                                                <li>Some asteroids are large enough that they actually contain moons of their own.</li>
+                                            </ol>
+                                        </div>
+
+                                        <div class="col-xs-12 col-sm-5 col-sm-pull-7">
+                                            <img class="img-responsive img-rounded" src="images/asteroid/opt/White_Dwarf_Asteroid_br.jpg">
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                </div>
+            </div>
+
+            <script src="js/menuFunctions.js"></script>
+    </body>
 
 </html>
