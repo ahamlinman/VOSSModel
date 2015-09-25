53dde273986befc99b9f956810e130fa796ffa68
Alex Hamlin <alexh1995@gmail.com>
2015-09-24 22:08:25 -0400

    Reformat sun page

--- sun.php
+++ sun.php
@@ -3,110 +3,123 @@
 
 <?php $orb_class = "active sun"; ?>
 
-<head>
-    <title>VOSS Model: Sun</title>
-    <?php include('fragments/head.html'); ?>
-</head>
+    <head>
+        <title>VOSS Model: Sun</title>
+        <?php include('fragments/head.html'); ?>
+    </head>
 
-<body>
-    <?php include( 'fragments/menu.html'); ?>
-	<div id="mainContainer">    
-	<div id="wrapper">
-				<?php include('fragments/load.html'); ?>
-        <section class="hero" style="background: url('images/sun/Cover.jpg');">
-            <div class="overlay">
-                <div class="container">
-                    <h1>The Sun</h1>
-                </div>
-            </div>
-        </section>
+    <body>
+        <?php include('fragments/menu.html'); ?>
+            <div id="mainContainer">
+                <div id="wrapper">
+                    <?php include('fragments/load.html'); ?>
+                        <section class="hero" style="background: url('images/sun/Cover.jpg');">
+                            <div class="overlay">
+                                <div class="container">
+                                    <h1>The Sun</h1>
+                                </div>
+                            </div>
+                        </section>
 
-        <section>
-           <div class="container">
-           <div class="row">
-               <div class="col-xs-12">
-                   <h2>The Big Picture</h2>
-               </div>
-           </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-7">
-                    <p>The sun is the reason there is life on Earth; without it, our planet would be an ice ball that could not sustain life. The sun’s heat and energy help warm the oceans, give life to plants that provide oxygen and food, create weather patterns, and mix our atmosphere! It makes up 99.8% of the mass and is the center of the solar system. The sun is the most important aspect in our solar system, yet, the sun is just a star. There are billions of other stars in the Milky Way Galaxy just like our sun. </p>
-                </div>
-                <div class="col-xs-12 col-sm-5">
-                    <img class="img-responsive img-rounded" src="images/sun/opt/BigPicture.jpg">
-                </div>
-            </div>
-            </div>
-        </section>
+                        <section>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <h2>The Big Picture</h2>
+                                    </div>
+                                </div>
 
-        <section>
-            <div class="container">
-               <div class="row">
-                   <div class="col-xs-12">
-                       <h2>History</h2>
-                   </div>
-               </div>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-7 col-sm-push-5">
-                        <p>The sun has been the center for many ancient cultures’ mythological stories. Cultures would even build stone structures to track the patterns of the sun and moon to anticipate seasons, calendars, and eclipses. Earlier cultures believed that the Earth was stagnant and the sun, other planets, and stars moved around the it. In 150 BCE, a Greek scholar Claudius Ptolemy wrote the Almagest that described this Earth-centered model. This idea was accepted until Nicolaus Copernicus published his idea of the heliocentric model, or sun-centered model, in On the Revolutions of the Celestial Spheres. </p>
-                        <p>There were many names for the sun in ancient cultures. The Ancient Romans called it Sol while the Greeks called it Helios. Latin, Italian, Portuguese, and Spanish all had forms of Sol as well. The modern term sun came from the German form of Sonne and the Dutch form of Zon.</p>
-                    </div>
-                    <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                        <img class="img-responsive img-rounded" src="images/sun/History.png">
-                    </div>
-                </div>
-            </div>
-        </section>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-7">
+                                        <p>The sun is the reason there is life on Earth; without it, our planet would be an ice ball that could not sustain life. The sun’s heat and energy help warm the oceans, give life to plants that provide oxygen and food, create weather patterns, and mix our atmosphere! It makes up 99.8% of the mass and is the center of the solar system. The sun is the most important aspect in our solar system, yet, the sun is just a star. There are billions of other stars in the Milky Way Galaxy just like our sun. </p>
+                                    </div>
 
-        <section>
-            <div class="container">
-               <div class="row">
-                   <div class="col-xs-12">
-                       <h2>Getting Technical</h2>
-                   </div>
-               </div>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-7">
-                        <p>Unlike Earth and the other inner planets in our solar system, the sun does not have a solid surface. It is a ball of gas made of 92% hydrogen and 8% helium. </p>
-                        <p>The sun is about 149.60 million kilometers or 92.96 million miles away from Earth. Due to the excessive distance between the sun and the Earth, it takes about 8 minutes 20 seconds for light to travel from the sun to the Earth. About one million Earths can fit inside of the sun.</p>
-                    </div>
-                    <div class="col-xs-12 col-sm-5">
-                        <img class="img-responsive img-rounded" src="images/sun/gettingtechnical1.jpg">
-                    </div>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-7 col-sm-push-5">
-                        <p>The sun has 6 different regions: the core, the radiative zone, the interior convective zone, the photosphere, the chromospehere, then the corona; each varying in size, temperature, and importance. The sun’s core is approximately 15 million degrees Celcius, or 27 million degrees Fahrenheit; allowing for thermonuclear fusion to take place. The energy produced in the core bounces around in the radiative zone for about 170,000 years before it gets to the convective zone. Convective zone temperatures fall to extreme lows, below 2 million degrees Celcius, due to the hot plasma bubbles moving upwards to the next layer of the sun. </p>
-                        <p>The photosphere is known as the sun's surface, which is where a majority of the radiation escapes, which we observe as heat and light on Earth. It is about 5500 degrees Celsius, or 10000 degrees Fahrenheit. Sunspots and solar flares appear on the photosphere as well. Sunspots are cooler, highly magnetic areas that are visible on the photosphere. Sunspot population changes every 11 years due to the cycle of magnetic activity on the sun. Solar flares and other huge mass ejections are also connected through this cycle. </p>
-                        <p>The chromosphere and corona lie above the photosphere. The light from these regions is rarely seen due to the level of brightness of the photosphere. However, during total eclipses of the sun, these two layers can be seen as a red glowing rim and a white halo encircling the sun.</p>
-                    </div>
-                    <div class="col-xs-12 col-sm-5 col-sm-pull-7">
-                        <img class="img-responsive img-rounded" src="images/sun/opt/gettingtechnical2.jpg">
-                    </div>
-                </div>
-            </div>
-        </section>
+                                    <div class="col-xs-12 col-sm-5">
+                                        <img class="img-responsive img-rounded" src="images/sun/opt/BigPicture.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
 
-        <section>
-            <div class="container">
-               <div class="row">
-                   <div class="col-xs-12">
-                       <h2>Missions</h2>
-                   </div>
-               </div>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-7">
-                        <p>In 1994, the Ulysses spacecraft was the first to observe the sun’s polar regions. In 2004, NASA’s Genesis spacecraft returned samples of solar wind. In 2007, NASA’s double-spacecraft Solar Terrestrial Relations Observatory returned the very first 3D images of the sun. Ulysses finally ended in 2009 and remains the only spacecraft to study the sun at high solar latitudes. In 2010, SDO was launched to observe the sun in high definition. In 2011, the STEREO spacecraft was able to see the entire sun for the very first time. </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-5">
-                        <img class="img-responsive img-rounded" src="images/sun/opt/Missions.jpg">
-                    </div>
+                        <section>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <h2>History</h2>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-7 col-sm-push-5">
+                                        <p>The sun has been the center for many ancient cultures’ mythological stories. Cultures would even build stone structures to track the patterns of the sun and moon to anticipate seasons, calendars, and eclipses. Earlier cultures believed that the Earth was stagnant and the sun, other planets, and stars moved around the it. In 150 BCE, a Greek scholar Claudius Ptolemy wrote the Almagest that described this Earth-centered model. This idea was accepted until Nicolaus Copernicus published his idea of the heliocentric model, or sun-centered model, in On the Revolutions of the Celestial Spheres. </p>
+
+                                        <p>There were many names for the sun in ancient cultures. The Ancient Romans called it Sol while the Greeks called it Helios. Latin, Italian, Portuguese, and Spanish all had forms of Sol as well. The modern term sun came from the German form of Sonne and the Dutch form of Zon.</p>
+                                    </div>
+
+                                    <div class="col-xs-12 col-sm-5 col-sm-pull-7">
+                                        <img class="img-responsive img-rounded" src="images/sun/History.png">
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <h2>Getting Technical</h2>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-7">
+                                        <p>Unlike Earth and the other inner planets in our solar system, the sun does not have a solid surface. It is a ball of gas made of 92% hydrogen and 8% helium. </p>
+
+                                        <p>The sun is about 149.60 million kilometers or 92.96 million miles away from Earth. Due to the excessive distance between the sun and the Earth, it takes about 8 minutes 20 seconds for light to travel from the sun to the Earth. About one million Earths can fit inside of the sun.</p>
+                                    </div>
+
+                                    <div class="col-xs-12 col-sm-5">
+                                        <img class="img-responsive img-rounded" src="images/sun/gettingtechnical1.jpg">
+                                    </div>
+                                </div>
+                                <br/> <!-- Leave this break here, it looks nicer -->
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-7 col-sm-push-5">
+                                        <p>The sun has 6 different regions: the core, the radiative zone, the interior convective zone, the photosphere, the chromospehere, then the corona; each varying in size, temperature, and importance. The sun’s core is approximately 15 million degrees Celcius, or 27 million degrees Fahrenheit; allowing for thermonuclear fusion to take place. The energy produced in the core bounces around in the radiative zone for about 170,000 years before it gets to the convective zone. Convective zone temperatures fall to extreme lows, below 2 million degrees Celcius, due to the hot plasma bubbles moving upwards to the next layer of the sun.</p>
+
+                                        <p>The photosphere is known as the sun's surface, which is where a majority of the radiation escapes, which we observe as heat and light on Earth. It is about 5500 degrees Celsius, or 10000 degrees Fahrenheit. Sunspots and solar flares appear on the photosphere as well. Sunspots are cooler, highly magnetic areas that are visible on the photosphere. Sunspot population changes every 11 years due to the cycle of magnetic activity on the sun. Solar flares and other huge mass ejections are also connected through this cycle.</p>
+
+                                        <p>The chromosphere and corona lie above the photosphere. The light from these regions is rarely seen due to the level of brightness of the photosphere. However, during total eclipses of the sun, these two layers can be seen as a red glowing rim and a white halo encircling the sun.</p>
+                                    </div>
+
+                                    <div class="col-xs-12 col-sm-5 col-sm-pull-7">
+                                        <img class="img-responsive img-rounded" src="images/sun/opt/gettingtechnical2.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <h2>Missions</h2>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-7">
+                                        <p>In 1994, the Ulysses spacecraft was the first to observe the sun’s polar regions. In 2004, NASA’s Genesis spacecraft returned samples of solar wind. In 2007, NASA’s double-spacecraft Solar Terrestrial Relations Observatory returned the very first 3D images of the sun. Ulysses finally ended in 2009 and remains the only spacecraft to study the sun at high solar latitudes. In 2010, SDO was launched to observe the sun in high definition. In 2011, the STEREO spacecraft was able to see the entire sun for the very first time. </p>
+                                    </div>
+
+                                    <div class="col-xs-12 col-sm-5">
+                                        <img class="img-responsive img-rounded" src="images/sun/opt/Missions.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
                 </div>
-            </div>
-        </section>
-</div>
-<script src="js/menuFunctions.js"></script>
-</body>
+                <script src="js/menuFunctions.js"></script>
+    </body>
 
 </html>
