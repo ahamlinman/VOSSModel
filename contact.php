d2e413dd034b7e69d1325703f4ca577afd1eefc6
Alex Hamlin <alexh1995@gmail.com>
2015-09-24 22:03:03 -0400

    Reformat contact page

--- contact.php
+++ contact.php
@@ -3,34 +3,36 @@
 
 <?php $orb_class = "inactive"; ?>
 
-	<head>
-		<title>VOSS Model: Contact Us</title>
-		<?php include('fragments/head.html'); ?>
-	</head>
-	
-	<body>
+    <head>
+        <title>VOSS Model: Contact Us</title>
+        <?php include('fragments/head.html'); ?>
+    </head>
+
+    <body>
         <?php include('fragments/menu.html'); ?>
-		<div id="mainContainer">
-		    <div id="wrapper">
-				<?php include('fragments/load.html'); ?>
-		    
-		        <section class="hero" style="background: none;">
-		                    <div class="overlay">
-		                        <div class="container">
-		                            <h1>Contact Us</h1>
-		                        </div>
-		                    </div>
-		                </section>
-		                <div class="container">
-		                    <div class="row">
-		                        <div class="col-xs-12">
-		                            <p>Questions, comments, or concerns? <a href="mailto:vossweb@purdue.edu">Email us at vossweb@purdue.edu!</a> Unfortunately, we cannot provide tours of the model. Please check back soon for information on self-guided tours.</p>
-		                        </div>
-		                    </div>
-		                </div>
-		    </div>
-		</div>
-		
-		<script src="js/menuFunctions.js"></script>
-	</body>
+            <div id="mainContainer">
+                <div id="wrapper">
+                    <?php include('fragments/load.html'); ?>
+
+                        <section class="hero" style="background: none;">
+                            <div class="overlay">
+                                <div class="container">
+                                    <h1>Contact Us</h1>
+                                </div>
+                            </div>
+                        </section>
+                        
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <p>Questions, comments, or concerns? <a href="mailto:vossweb@purdue.edu">Email us at vossweb@purdue.edu!</a> Unfortunately, we cannot provide tours of the model. Please check back soon for information on self-guided tours.</p>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+
+            <script src="js/menuFunctions.js"></script>
+    </body>
+
 </html>
